ac6c07b53a689f1c9840d8342bfbbffeb9690f87
Fabian Wiles <fabianwiles@live.com>
2020-01-02 15:49:54 +1300

    fixup! nestjs tests

--- examples/nestjs/src/main.ts
+++ examples/nestjs/src/main.ts
@@ -1,10 +1,11 @@
 import {Logger} from '@nestjs/common';
 import {NestFactory} from '@nestjs/core';
+import {ExpressAdapter} from '@nestjs/platform-express';
 
 import {AppModule} from './app.module';
 
 async function bootstrap(port: number) {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter());
   await app.listen(port);
   Logger.log(`Application served at http://localhost:${port}`);
 }
